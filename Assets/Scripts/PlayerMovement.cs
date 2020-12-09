4e50aec4dffc4a4934751fdaabe9e8f580577206
Markéta Machová <machova.marketa.tu@gmail.com>
2020-12-09 19:54:34 +0100

    Init project

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PathCreation;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public GameObject player;
+    public PathCreator pathCreator;
+    public float speed;
+    float distance;
+
+    void Start()
+    {
+        player.transform.position = pathCreator.path.GetPoint(10);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        distance += speed * Time.deltaTime;
+        player.transform.position = pathCreator.path.GetPointAtDistance(distance);
+    }
+}
