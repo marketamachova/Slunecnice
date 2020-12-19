dce25d62aa81f717a62b54ae638590191ef74373
marketamachova <machoma8@fel.cvut.cz>
2020-12-19 17:49:41 +0100

    Add support for control application

---
+++ Assets/Scripts/NetworkController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class NetworkController : NetworkBehaviour
+{
+    private void Update()
+    {
+        if (isLocalPlayer && Input.GetKeyDown(KeyCode.Space)) {
+            Debug.Log("Pressed Start");
+            StartTravel();
+        }
+    }
+
+    public override void OnStartServer()
+    {
+        Debug.Log("Server has started");
+    }
+
+    [Command]
+    private void StartTravel()
+    {
+        Debug.Log("Starting travel");
+    }
+
+}
+
