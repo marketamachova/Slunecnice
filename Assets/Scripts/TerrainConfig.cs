70b4c43540df6ef23515a0f23de66864526d5284
marketamachova <machoma8@fel.cvut.cz>
2020-12-21 14:57:05 +0100

    Add nature elements ě

---
+++ Assets/Scripts/TerrainConfig.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainConfig : MonoBehaviour
+{
+    public float distance;
+
+    void Start()
+    {
+        //Terrain terrain = (Terrain) GameObject.FindObjectOfType(typeof(Terrain));
+        //terrain.treeDistance = distance;
+
+        //var terrainScripts = GameObject.FindSceneObjectsOfType(typeof(Terrain));
+
+        //foreach (var terrain in terrainScripts)
+        //{
+        //    (terrain as Terrain).treeDistance = 2000;
+        //}
+
+        Terrain.activeTerrain.treeDistance = distance;
+
+    }
+
+
+}
