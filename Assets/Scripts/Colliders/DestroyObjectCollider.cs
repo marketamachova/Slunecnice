d523eb5cbcca7e0586b7acd71103e356c098715d
Markéta Machová <machova.marketa.tu@gmail.com>
2021-05-02 16:42:35 +0200

    Optimisation

---
+++ Assets/Scripts/Colliders/DestroyObjectCollider.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Colliders
+{
+    public class DestroyObjectCollider : MonoBehaviour
+    {
+        [SerializeField] private GameObject objectToDestroy;
+        [SerializeField] private GameObject objectToSetActive;
+
+        public void OnCollisionEnter(Collision collision)
+        {
+            if (collision.gameObject.CompareTag("NetworkCamera") && !collision.gameObject.CompareTag("Terrain") && gameObject.name != "Terrain")
+            {
+                Destroy(objectToDestroy);
+                if (objectToSetActive != null)
+                {
+                    objectToSetActive.SetActive(true);
+                }
+            }
+        }
+    }
+}
